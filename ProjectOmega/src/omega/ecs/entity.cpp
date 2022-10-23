3dfab0ce7d6a136934a673cf0cae1d4a8d7ec549
TigerCipher <matt.rogers8396@gmail.com>
2022-10-23 16:12:33 -0500

    implemented code for entity and component

---
+++ ProjectOmega/src/omega/ecs/entity.cpp
@@ -0,0 +1,79 @@
+// ------------------------------------------------------------------------------
+//
+// ProjectOmega
+//    Copyright 2022 Matthew Rogers
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+// File Name: entity.cpp
+// Date File Created: 10/23/2022
+// Author: Matt
+//
+// ------------------------------------------------------------------------------
+
+#include "entity.h"
+
+#include "component.h"
+#include "omega/core/game.h"
+
+namespace omega
+{
+entity::entity(game* game) : m_state(ACTIVE), m_game(game), m_position({0, 0}), m_scale(1.0f), m_rotation(0.0f)
+{
+    m_game->add_entity(this);
+}
+
+entity::~entity()
+{
+    m_game->remove_entity(this);
+    while (!m_components.empty())
+    {
+        delete m_components.back();
+    }
+}
+
+void entity::update(f32 delta)
+{
+    if (m_state != ACTIVE) return;
+    update_components(delta);
+    update_entity(delta);
+}
+
+void entity::update_components(f32 delta)
+{
+    for (auto* comp : m_components)
+    {
+        comp->update(delta);
+    }
+}
+
+void entity::update_entity(f32 delta) {}
+
+void entity::add_component(component* comp)
+{
+    s32  order = comp->get_update_order();
+    auto it    = m_components.begin();
+    while (it++ != m_components.end())
+    {
+        if (order < (*it)->get_update_order()) break;
+    }
+
+    m_components.insert(it, comp);
+}
+
+void entity::remove_component(component* comp)
+{
+    auto it = std::find(m_components.begin(), m_components.end(), comp);
+    if (it != m_components.end()) m_components.erase(it);
+}
+} // namespace omega
